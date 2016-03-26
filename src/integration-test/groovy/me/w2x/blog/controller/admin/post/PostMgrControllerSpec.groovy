41110c4273caa96c3bd5797a4d34006eac19ad1c
wancaibida <wancaibida@gmail.com>
2016-03-26 22:56:26 +0800

    Add integration test...

---
+++ src/integration-test/groovy/me/w2x/blog/controller/admin/post/PostMgrControllerSpec.groovy
@@ -0,0 +1,44 @@
+package me.w2x.blog.controller.admin.post
+
+import grails.test.mixin.TestFor
+import grails.test.mixin.integration.Integration
+import grails.transaction.Rollback
+import me.w2x.blog.command.PostCommand
+import me.w2x.blog.domain.Post
+import me.w2x.blog.enu.PostStatus
+import spock.lang.Specification
+
+import javax.servlet.http.HttpServletResponse
+
+@Integration
+@Rollback
+@TestFor(PostMgrController)
+class PostMgrControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test add"() {
+        def postCommand = new PostCommand()
+        given:
+        controller.request.method = 'post'
+        postCommand.with {
+            title = 'test post 000'
+            categoryId = 1L
+            postStatus = PostStatus.PUBLISH.key
+            raw = 'raw text'
+            content = 'content text'
+        }
+
+        when:
+        controller.add(postCommand)
+
+        then:
+        controller.response.status == HttpServletResponse.SC_OK
+        Post.findByTitle('test post 000')
+        Post.findByTitle('test post 000')?.postStatus == PostStatus.PUBLISH.key
+    }
+}
