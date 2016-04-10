ef17119cc20af04175d85ae277419880a6af27e6
wancaibida <wancaibida@gmail.com>
2016-04-10 15:25:38 +0800

    integration test...

---
+++ src/integration-test/groovy/me/w2x/blog/PostMgrServiceSpec.groovy
@@ -0,0 +1,37 @@
+package me.w2x.blog
+
+
+import grails.test.mixin.integration.Integration
+import grails.transaction.*
+import me.w2x.blog.command.PostCommand
+import me.w2x.blog.domain.Post
+import me.w2x.blog.enu.PostStatus
+import me.w2x.blog.service.PostMgrService
+import org.springframework.beans.factory.annotation.Autowired
+import spock.lang.*
+
+@Integration
+@Rollback
+class PostMgrServiceSpec extends Specification {
+
+    @Autowired
+    PostMgrService postMgrService
+
+    void "test add post"() {
+        setup:
+        def command = new PostCommand()
+        command.with {
+            title = 'test title 000'
+            content = 'content 000'
+            raw = 'raw 000'
+            categoryId = 1L
+            postStatus = PostStatus.PUBLISH.key
+        }
+
+        when:
+        postMgrService.add(command)
+
+        then:
+        Post.findByTitle('test title 000')?.content == 'content 000'
+    }
+}
