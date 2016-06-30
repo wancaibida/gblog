6090adfa96047730ea0f90c92cc0370cc3c7d00b
wancaibida <wancaibida@gmail.com>
2016-06-30 22:02:41 +0800

    change url format...

--- grails-app/controllers/UrlMappings.groovy
+++ grails-app/controllers/UrlMappings.groovy
@@ -21,78 +21,60 @@ class UrlMappings {
                 "/authsuccess"(controller: "user", action: "authsuccess", method: "GET")
             }
 
-            "/allMenus"(controller: "index") {
-                action = [GET: 'allMenus', POST: 'allMenus']
-            }
+            "/allMenus"(controller: "index", action: 'allMenus', method: 'GET')
 
-            "/menu"(controller: "index") {
-                action = [GET: 'allMenus', POST: 'allMenus']
-            }
+            "/menu"(controller: "index", action: 'allMenus', method: 'GET')
 
             "/buttons"(controller: "index", action: "buttons", method: "GET")
-            "/menus"(controller: "menuMgr") {
-                action = [GET: 'index', POST: 'add']
-            }
-            "/menus/${menuId}"(controller: "menuMgr") {
-                action = [POST: 'update', DELETE: 'delete']
-            }
-            "/menus/parents"(controller: "menuMgr") {
-                action = [GET: 'parents']
-            }
-            "/menus/${menuId}/buttons"(controller: "menuMgr") {
-                action = [GET: 'buttons', POST: 'addButton']
-            }
-            "/menus/${menuId}/buttons/${buttonId}"(controller: "menuMgr") {
-                action = [POST: 'updateButton', DELETE: 'deleteButton']
-            }
-            "/menus/${menuId}/buttons/all"(controller: "menuMgr") {
-                action = [POST: 'addButtons']
-            }
-            "/categorys"(controller: "categoryMgr") {
-                action = [GET: 'index', POST: 'add']
-            }
-            "/categorys/parents"(controller: "categoryMgr") {
-                action = [GET: 'parents']
-            }
-            "/categorys/${categoryId}"(controller: "categoryMgr") {
-                action = [POST: 'update', DELETE: 'delete']
-            }
 
-            "/posts"(controller: "postMgr") {
-                action = [GET: 'index', POST: 'add']
-            }
-            "/posts/${postId}"(controller: "postMgr") {
-                action = [POST: 'update', DELETE: 'delete']
-            }
-            "/posts/view"(controller: "postMgr") {
-                action = [GET: 'view']
-            }
+            "/menus"(controller: "menuMgr", action: 'index', method: 'GET')
+            "/menus"(controller: "menuMgr", action: 'add', method: 'POST')
 
-            "/drafts"(controller: "postMgr", action: 'saveDraft', method: 'post')
+            "/menus/${menuId}"(controller: "menuMgr", action: 'update', method: 'POST')
+            "/menus/${menuId}"(controller: "menuMgr", action: 'delete', method: 'DELETE')
 
-            "/drafts/$draftId"(controller: "postMgr", action: 'getDraft', method: 'get')
+            "/menus/parents"(controller: "menuMgr", action: 'parents', method: 'GET')
 
-            "/password/reset"(controller: 'user') {
-                action = [POST: 'updatePassword']
-            }
+            "/menus/${menuId}/buttons"(controller: "menuMgr", action: 'buttons', method: 'GET')
+            "/menus/${menuId}/buttons"(controller: "menuMgr", action: 'addButton', method: 'POST')
+
+            "/menus/${menuId}/buttons/${buttonId}"(controller: "menuMgr", action: 'updateButton', method: 'POST')
+            "/menus/${menuId}/buttons/${buttonId}"(controller: "menuMgr", action: 'deleteButton', method: 'DELETE')
+
+            "/menus/${menuId}/buttons/all"(controller: "menuMgr", action: 'addButtons', method: 'POST')
+
+            "/categorys"(controller: "categoryMgr", action: 'index', method: 'GET')
+            "/categorys"(controller: "categoryMgr", action: 'add', method: 'POST')
+
+            "/categorys/parents"(controller: "categoryMgr", action: 'parents', method: 'GET')
+
+            "/categorys/${categoryId}"(controller: "categoryMgr", action: 'update', method: 'POST')
+            "/categorys/${categoryId}"(controller: "categoryMgr", action: 'delete', method: 'DELETE')
+
+
+            "/posts"(controller: "postMgr", action: 'index', method: 'GET')
+            "/posts"(controller: "postMgr", action: 'add', method: 'POST')
+
+            "/posts/${postId}"(controller: "postMgr", action: 'update', method: 'POST')
+            "/posts/${postId}"(controller: "postMgr", action: 'delete', method: 'DELETE')
+
+            "/posts/view"(controller: "postMgr", action: 'view', method: 'GET')
+
+            "/drafts"(controller: "postMgr", action: 'saveDraft', method: 'POST')
+
+            "/drafts/$draftId"(controller: "postMgr", action: 'getDraft', method: 'GET')
+
+            "/password/reset"(controller: 'user', action: 'updatePassword', method: 'POST')
         }
 
 
-        "/posts"(controller: 'post') {
-            action = [GET: 'index']
-        }
+        "/posts"(controller: 'post', action: 'index', method: 'GET')
 
-        "/posts/${postId}"(controller: 'post') {
-            action = [GET: 'view']
-        }
+        "/posts/${postId}"(controller: 'post', action: 'view', method: 'GET')
 
-        "/posts/date/${year}/${month}"(controller: 'post') {
-            action = [GET: 'index']
-        }
+        "/posts/date/${year}/${month}"(controller: 'post', action: 'index', method: 'GET')
 
-        "/categorys/${alias}"(controller: 'post') {
-            action = [GET: 'index']
-        }
+        "/categorys/${alias}"(controller: 'post', action: 'index', method: 'GET')
 
         "/about"(controller: 'other', method: 'GET', action: 'about')
 
